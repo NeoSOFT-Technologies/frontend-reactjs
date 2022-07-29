f3aebd2004c9a62dc5de4bffa23745e00f48a9ef
“mihir <90671722+mihir-neosoft@users.noreply.github.com>
2022-07-29 10:45:11 +0530

    fixed Security Hotspot

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -28,7 +28,8 @@ precacheAndRoute(self.__WB_MANIFEST);
 // Set up App Shell-style routing, so that all navigation requests
 // are fulfilled with your index.html shell. Learn more at
 // https://developers.google.com/web/fundamentals/architecture/app-shell
-const fileExtensionRegexp = /\/[^/?]+\.[^/]+$/;
+// eslint-disable-next-line unicorn/better-regex, no-useless-escape
+const fileExtensionRegexp = /\/[^\/?]+\.[^\/]+$/;
 registerRoute(
   // Return false to exempt requests from being fulfilled by index.html.
   ({ request, url }: { request: Request; url: URL }) => {
