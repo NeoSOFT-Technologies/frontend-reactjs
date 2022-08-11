d60dc55ef59c4aa86320dd8cec3eb7fd06f0026f
DeepthiPolsani <deepthi.polsani@neosoftmail.com>
2022-08-11 10:59:47 +0530

    converted crypto to class

---
+++ config-overrides.js
@@ -0,0 +1,17 @@
+/* eslint-disable unicorn/prefer-module */
+// final version of the file
+module.exports = function override(config) {
+  const fallback = config.resolve.fallback || {};
+  Object.assign(fallback, {
+    crypto: require.resolve("crypto-browserify"),
+    stream: require.resolve("stream-browserify"),
+    assert: require.resolve("assert"),
+    http: require.resolve("stream-http"),
+    https: require.resolve("https-browserify"),
+    os: require.resolve("os-browserify"),
+    url: require.resolve("url"),
+  });
+  config.resolve.fallback = fallback;
+  config.ignoreWarnings = [/Failed to parse source map/];
+  return config;
+};
--- src/services/crypto/crypto.ts
+++ src/services/crypto/crypto.ts
@@ -1,25 +1,34 @@
-import * as crypto from "node:crypto";
+// eslint-disable-next-line unicorn/prefer-node-protocol
+import * as crypto from "crypto";
+const ALGORITHM = "aes-256-gcm";
+class Crypto {
+  // algorithm - AES 256 GCM Mode
+  // private ALGORITHM = "aes-256-gcm";
 
-// algorithm - AES 256 GCM Mode
-const algorithm = "aes-256-gcm";
+  // iterations: It must be a number and should be set as high as possible.
+  // So, the more is the number of iterations, the more secure the derived key will be,
+  // but in that case it takes greater amount of time to complete.
+  // number of interation - the value of 2145 is randomly chosen
+  private ITERATIONS = 2145;
 
-// iterations: It must be a number and should be set as high as possible.
-// So, the more is the number of iterations, the more secure the derived key will be,
-// but in that case it takes greater amount of time to complete.
-// number of interation - the value of 2145 is randomly chosen
-const iterations = 2145;
+  // keylen: It is the key of the required byte length and it is of type number.
+  // derive encryption key: 32 byte key length
+  private KEYLEN = 32;
 
-// keylen: It is the key of the required byte length and it is of type number.
-// derive encryption key: 32 byte key length
-const keylen = 32;
+  // digest: It is a digest algorithms of string type.
+  private DIGEST = "sha512";
 
-// digest: It is a digest algorithms of string type.
-const digest = "sha512";
+  // salt
+  private SALT = crypto.randomBytes(64);
+  private static instance: Crypto;
+  public static getInstance() {
+    if (!this.instance) {
+      this.instance = new Crypto();
+    }
+    return this.instance;
+  }
 
-// salt
-const salt = crypto.randomBytes(64);
-
-export const encrypt = (data: string, secretKey: string): string => {
+  public encrypt = (data: string, secretKey: string): string => {
     // constant to encrypt the data
     const inputEncoding = "utf8";
     const outputEncoding = "base64";
@@ -30,10 +39,10 @@ export const encrypt = (data: string, secretKey: string): string => {
     // The method gives an asynchronous Password-Based Key Derivation
     const key: Buffer = crypto.pbkdf2Sync(
       secretKey,
-      salt,
-      iterations,
-      keylen,
-      digest
+      this.SALT,
+      this.ITERATIONS,
+      this.KEYLEN,
+      this.DIGEST
     );
 
     // create a Cipher object, with the stated algorithm, key and initialization vector (iv).
@@ -41,7 +50,7 @@ export const encrypt = (data: string, secretKey: string): string => {
     // @key
     // @iv
     // @options
-    const cipher = crypto.createCipheriv(algorithm, key, iv);
+    const cipher = crypto.createCipheriv(ALGORITHM, key, iv);
 
     // create a Cipher object, with the stated algorithm, key and initialization vector (iv).
     // @algorithm - AES 256 GCM Mode
@@ -59,15 +68,11 @@ export const encrypt = (data: string, secretKey: string): string => {
     const tag = cipher.getAuthTag();
 
     // return [enc1, enc2, iv, tag];
-    const encryptedData = Buffer.concat([enc1, enc2, iv, tag]).toString(
-      outputEncoding
-    );
-
     // return the result
-    return encryptedData;
-};
+    return Buffer.concat([enc1, enc2, iv, tag]).toString(outputEncoding);
+  };
 
-export const decrypt = (data: string, secretKey: string): string => {
+  public decrypt = (data: string, secretKey: string): string => {
     // constant to decrypt the data
     const inputEncoding = "base64";
     const outputEncoding = "utf8";
@@ -77,7 +82,13 @@ export const decrypt = (data: string, secretKey: string): string => {
     const bufferData = Buffer.from(data, inputEncoding);
 
     // derive key using; 32 byte key length
-    const key = crypto.pbkdf2Sync(secretKey, salt, iterations, keylen, digest);
+    const key = crypto.pbkdf2Sync(
+      secretKey,
+      this.SALT,
+      this.ITERATIONS,
+      this.KEYLEN,
+      this.DIGEST
+    );
 
     // extract iv from encrypted data
     const iv = bufferData.slice(-28, -16);
@@ -89,7 +100,7 @@ export const decrypt = (data: string, secretKey: string): string => {
     const text = bufferData.slice(0, -28);
 
     // AES 256 GCM Mode
-    const decipher = crypto.createDecipheriv(algorithm, key, iv);
+    const decipher = crypto.createDecipheriv(ALGORITHM, key, iv);
 
     // set the auth tag
     decipher.setAuthTag(tag);
@@ -106,4 +117,7 @@ export const decrypt = (data: string, secretKey: string): string => {
 
     // parse the string decrypted data
     return str;
-};
+  };
+}
+
+export default Crypto;
